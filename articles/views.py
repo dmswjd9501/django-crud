5a9d3ec58d5db4dd05e2e7c41d72233e5e60b9a6
dmswjd9501 <dmswjd9501@soongsil.ac.kr>
2019-09-10 11:47:24 +0900

    10 | Update Form GET to POST

--- articles/views.py
+++ articles/views.py
@@ -14,8 +14,8 @@ def new(request):
     
 def create(request):
    # 저장 로직
-   title = request.GET.get('title')
-   content = request.GET.get('content')
+   title = request.POST.get('title')
+   content = request.POST.get('content')
    article = Article(title=title, content=content)
    article.save()
    context = {
@@ -45,8 +45,8 @@ def edit(request, article_pk):
 
 def update(request, article_pk):
     article = Article.objects.get(pk=article_pk)
-    article.title = request.GET.get('title')
-    article.content = request.GET.get('content')
+    article.title = request.POST.get('title')
+    article.content = request.POST.get('content')
     article.save()
     return redirect('articles:detail', article.pk)
 

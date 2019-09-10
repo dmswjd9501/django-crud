65bfa05a7899c34bb48925a512b578b24de301e1
dmswjd9501 <dmswjd9501@soongsil.ac.kr>
2019-09-10 15:20:53 +0900

    11 | Update create, update, delete function

--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+
 from .models import Article
 
 # Create your views here.
@@ -9,19 +10,20 @@ def index(request):
     }
     return render(request, 'articles/index.html', context)
 
-def new(request):
-    return render(request, 'articles/new.html')
+# def new(request):
+#     return render(request, 'articles/new.html')
     
 def create(request):
    # 저장 로직
-   title = request.POST.get('title')
-   content = request.POST.get('content')
-   article = Article(title=title, content=content)
-   article.save()
-   context = {
-       'article': article
-   }
-   return redirect('articles:detail', article.pk)
+    if request.method == 'GET':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        article = Article(title=title, content=content)
+        article.save()
+        return redirect('articles:detail', article.pk)
+    else:
+        return render(request, 'articles/new.html')
+
     # return render(request, 'articles/create.html', context)
 
 def detail(request, article_pk):
@@ -31,23 +33,33 @@ def detail(request, article_pk):
     }
     return render(request, 'articles/detail.html', context)
 
+from django.views.decorators.http import require_POST
+
+@require_POST
 def delete(request, article_pk):
     article = Article.objects.get(pk=article_pk)
+    # if request.method == 'POST':
     article.delete()
     return redirect('articles:index')
+    # else:
+    #     return redirect('articles:detail', article.pk)
 
-def edit(request, article_pk):
-    article = Article.objects.get(pk=article_pk)
-    context = {
-        'article' : article
-    }
-    return render(request, 'articles/edit.html', context)
+# def edit(request, article_pk):
+#     article = Article.objects.get(pk=article_pk)
+#     context = {
+#         'article' : article
+#     }
+#     return render(request, 'articles/edit.html', context)
 
 def update(request, article_pk):
     article = Article.objects.get(pk=article_pk)
-    article.title = request.POST.get('title')
-    article.content = request.POST.get('content')
-    article.save()
-    return redirect('articles:detail', article.pk)
-
-
+    if request.method == 'POST':
+        article.title = request.POST.get('title')
+        article.content = request.POST.get('content')
+        article.save()
+        return redirect('articles:detail', article.pk)
+    else:
+        context = {
+            'article' : article
+        }
+    return render(request, 'articles/edit.html', context)

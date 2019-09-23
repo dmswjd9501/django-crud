6f59662c81a8d7fd450cb467e88e0674801f86b4
dmswjd9501 <dmswjd9501@soongsil.ac.kr>
2019-09-23 15:32:40 +0900

    23 | Add thumbnail with django-imagekit

--- articles/models.py
+++ articles/models.py
@@ -1,37 +1,45 @@
 from django.db import models
-
+from imagekit.models import ProcessedImageField, ImageSpecField
+from imagekit.processors import ResizeToFill
 # Create your models here.
 # 1. 모델(스키마) 정의
-# 데이터베이스 테이블을 정의하고, 
+# 데이터베이스 테이블을 정의하고,
 # 각각의 컬럼(필드) 정의
 class Article(models.Model):
-    # id : integer 자동으로 정의(Primary Key)
-    # id = models.AutoField(primary_key=True) -> Integer 값이 자동으로 하나씩 증가(AUTOINCREMENT)
-    # Charfield = 필수인자로 max_length 지정
-    title = models.CharField(max_length=10)
-    content = models.TextField()
-    image = models.ImageField(blank=True)
-    # DatetimeField
-    #   auto_now_add : 생성시 자동으로 입력
-    #   auto_now : 수정시 자동으로 저장
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return f'{self.id} - {self.title}'
-
+   # id : integer 자동으로 정의(Primary Key)
+   # id = models.AutoField(primary_key=True) -> Integer 값이 자동으로 하나씩 증가(AUTIINCREMENT)
+   # CharField - 필수인자로 max_length 지정
+   title = models.CharField(max_length=10)
+   content = models.TextField()
+   image = models.ImageField(blank=True)
+   # ImageSpecField : Input 하나만 받고 잘라서 저장
+   # ProcessedImageField: Input 받은 것을 처리해서 저장
+   # resize to fill : 300 * 300
+   # resize to fit : 긴쪽을(너비 혹은 높이) 300에 맞추고 비율에 맞게 자름
+   image_thumbnail = ProcessedImageField(
+       blank=True,
+       processors=[ResizeToFill(300, 300)],
+       format='JPEG',
+       options={'quality': 80}
+   )
+   # DateTimeField
+   #   auto_now_add : 생성시 자동으로 저장 (한 번)
+   #   auto_now : 수정시마다 자동으로 저장
+   created_at = models.DateTimeField(auto_now_add=True)
+   updated_at = models.DateTimeField(auto_now=True)
+   def __str__(self):
+       return f'{self.id} : {self.title}'
 class Comment(models.Model):
-    content = models.CharField(max_length=140)
-    created_at = models.DateTimeField(auto_now_add=True)
-    article = models.ForeignKey(Article, on_delete=models.CASCADE)
-    # on_delete
-    # 1. CASCADE : 글이 삭제되었을 떄 모든 댓글을 삭제
-    # 2. PROTECT : 댓글이 존재하면 글 삭제 안됨.
-    # 3. SET_NULL : 글이 삭제되면 NULL로 치환(NOT NULL일 경우 옵션 사용X)
-    # 4. SET_DEFAULT : 디폴트 값으로 치환.
-
+   content = models.CharField(max_length=140)
+   created_at = models.DateTimeField(auto_now_add=True)
+   article = models.ForeignKey(Article, on_delete=models.CASCADE)
+   # on_delete
+   # 1. CASCADE : 글이 삭제되었을 때 모든 댓글을 삭제
+   # 2. PROTECT : 댓글이 존재하면 글 삭제 안됨.
+   # 3. SET_NULL : 글이 삭제되면 NULL로 치환(NOT NULL일 경우 옵션 사용X)
+   # 4. SET_DEFAULT : 디폴트 값으로 치환.
 # models.py : python 클래스 정의
-#           : 모델 설계도
-# makemigrations: migration 파일 생성
-#           : DB 설계도 작성
+#               -> 모델 설계도
+# makemigrations : migration 파일 생성
+#                   -> DB 설계도 작성
 # migrate : migration 파일 DB 반영
--- crud/settings.py
+++ crud/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles', # static file css
     'django_extensions',
     'bootstrap4',
+    'imagekit',
 ]
 
 MIDDLEWARE = [

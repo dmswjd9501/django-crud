18562e661dfa66c18cef2400313cf770452facc3
dmswjd9501 <dmswjd950101@soongsil.ac.kr>
2019-10-15 14:09:35 +0900

    26 | Accounts login

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
-# from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login as auth_login
+
+
 # Create your views here.
 def signup(request):
     if request.method == 'POST':
@@ -18,3 +20,17 @@ def signup(request):
     # 만약 POST 요청 -> 검증 실패하면 6 7 8 14 15 16 17 순서대로 실행 
     # 만약 GET 요청 -> 11 12 14 15 16 17 실행
     # 그때 form은 7번 라인의 request.POST 값 그대로 나타내주게!
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST) # 인자의 순서가 다름 / request에 사용자이름, 쿠키같은게 들어져서 옴
+        if form.is_valid():
+            user = form.get_user() # user 인스턴스를 가지고 와서
+            auth_login(request, user) # login 시켜주는 함수에 넣어주자
+            return redirect('articles:index')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/login.html', context)
--- articles/views.py
+++ articles/views.py
@@ -12,7 +12,6 @@ from .models import Article, Comment
 @require_GET
 def index(request):
     articles = Article.objects.order_by('-id')
-    embed()
     context = {
         'articles': articles
     }
